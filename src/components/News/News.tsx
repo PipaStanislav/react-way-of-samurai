4de218aad2af0e011e35e5b708599678a13268a9
Stanislav Pipa <stanislav.pipa@sigma.software>
2022-10-24 14:13:57 +0300

    Added types/interfaces for News component

--- src/components/News/News.tsx
+++ src/components/News/News.tsx
@@ -1,6 +1,9 @@
 import styles from './News.module.css';
+import { FC } from 'react';
 
-const News = () => {
+type ParamsType = {}
+
+const News: FC<ParamsType> = (params): JSX.Element => {
   return (
     <div className={ styles.news }>
       <iframe className={styles.newsIframe} src="https://www.bbc.com/" title={ 'News' }></iframe>
